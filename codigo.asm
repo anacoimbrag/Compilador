3068a7d32312809cc9e7e13433c159cbc72dbe97
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2015-11-14 14:07:08 -0200

    Mudanca no .asm

--- codigo.asm
+++ codigo.asm
@@ -1,16 +1,16 @@
-sseg SEGMENT STACK ;início seg. pilha
+sseg SEGMENT STACK ;inï¿½cio seg. pilha
 byte 4000h DUP(?) ;dimensiona pilha
 sseg ENDS ;fim seg. pilha
 
-dseg SEGMENT PUBLIC ;início seg. dados
-byte 4000h DUP(?) ;temporários
+dseg SEGMENT PUBLIC ;inï¿½cio seg. dados
+byte 4000h DUP(?) ;temporï¿½rios
 sword ? ;inteiro n
 byte 100h DUP(?) ;string nome
 byte ? ;logico naoterminou
 byte 10; valor positivo maxiter
 dseg ENDS ;fim seg. dados
 
-cseg SEGMENT PUBLIC ;início seg. código
+cseg SEGMENT PUBLIC ;inï¿½cio seg. cï¿½digo
 ASSUME CS:cseg, DS:dseg
 strt:
 mov ax, dseg
@@ -18,170 +18,171 @@ mov ds, ax
 mov di, 0 ;end. string temp.
 mov cx, 0 ;contador
 cmp ax,0 ;verifica sinal
-jge R0 ;salta se número positivo
-mov bl, 2Dh ;senão, escreve sinal –
+jge R0 ;salta se nï¿½mero positivo
+mov bl, 2Dh ;senï¿½o, escreve sinal ï¿½
 mov ds:[di], bl
-add di, 1 ;incrementa índice
-neg ax ;toma módulo do número
+add di, 1 ;incrementa ï¿½ndice
+neg ax ;toma mï¿½dulo do nï¿½mero
 R0:
-mov bx, 10 ;divisor
+  mov bx, 10 ;divisor
 R1:
-add cx, 1 ;incrementa contador
-mov dx, 0 ;estende 32bits p/ div.
-idiv bx ;divide DXAX por BX
-push dx ;empilha valor do resto
-cmp ax, 0 ;verifica se quoc. é 0
-jne R1 ;se não é 0, continua
-;agora, desemp. os valores e escreve o string
+  add cx, 1 ;incrementa contador
+  mov dx, 0 ;estende 32bits p/ div.
+  idiv bx ;divide DXAX por BX
+  push dx ;empilha valor do resto
+  cmp ax, 0 ;verifica se quoc. ï¿½ 0
+  jne R1 ;se nï¿½o ï¿½ 0, continua
+  ;agora, desemp. os valores e escreve o string
 R2:
-pop dx ;desempilha valor
-add dx, 30h ;transforma em caractere
-mov ds:[di],dl ;escreve caractere
-add di, 1 ;incrementa base
-add cx, -1 ;decrementa contador
-cmp cx, 0 ;verifica pilha vazia
-jne R2 ;se não pilha vazia, loop
-;grava fim de string
-mov dl, 024h ;fim de string
-mov ds:[di], dl ;grava '$'
-;exibe string
-mov dx, 0
-mov ah, 09h
-int 21h
+  pop dx ;desempilha valor
+  add dx, 30h ;transforma em caractere
+  mov ds:[di],dl ;escreve caractere
+  add di, 1 ;incrementa base
+  add cx, -1 ;decrementa contador
+  cmp cx, 0 ;verifica pilha vazia
+  jne R2 ;se nï¿½o pilha vazia, loop
+  ;grava fim de string
+  mov dl, 024h ;fim de string
+  mov ds:[di], dl ;grava '$'
+  ;exibe string
+  mov dx, 0
+  mov ah, 09h
+  int 21h
 
 
-dseg SEGMENT PUBLIC
-byte "digite seu nome$"
-dseg ENDS
+  dseg SEGMENT PUBLIC
+  byte "digite seu nome$"
+  dseg ENDS
 
-mov dx, 0
-mov al, 0FFh
-mov ds:[0], al
-mov ah, 0Ah
-int 21h
+  mov dx, 0
+  mov al, 0FFh
+  mov ds:[0], al
+  mov ah, 0Ah
+  int 21h
 
-mov ah, 02h
-mov dl, 0Dh
-int 21h
-mov DL, 0Ah
-int 21h
+  mov ah, 02h
+  mov dl, 0Dh
+  int 21h
+  mov DL, 0Ah
+  int 21h
 
-mov di, 02;posição do string
-mov si, 16386mov ax, 0 ;acumulador
-mov cx, 10 ;base decimal
-mov dx, 1 ;valor sinal +
-mov bh, 0
-mov bl, ds:[di] ;caractere
-cmp bx, 2Dh ;verifica sinal
-jne R0 ;se não negativo
-mov dx, -1 ;valor sinal -
-add di, 1 ;incrementa base
-mov bl, ds:[di] ;próximo caractere
-R0:
-push dx ;empilha sinal
-mov dx, 0 ;reg. multiplicação
-R1:
-cmp bx, 0dh ;verifica fim string
-je R2 ;salta se fim string
-imul cx ;mult. 10
-add bx, -48 ;converte caractere
-add ax, bx ;soma valor caractere
-add di, 1 ;incrementa base
-mov bh, 0
-mov bl, ds:[di] ;próximo caractere
-jmp R1 ;loop
-R2:
-pop cx ;desempilha sinal
-imul cx ;mult. sinal
-
-mov al, 0FFh ; const true
-mov DS:[0], al
-mov al, DS:[0]
-mov DS:[16642], ax
-mov al, 0 ; const 0
-mov DS:[0], al
-mov al, DS:[0]
-mov ah, 0
-mov DS:[16384], ax
-R0:
-mov ax, DS:[16642]
-cmp ax, 0
-je R1
-mov di, 0 ;end. string temp.
-mov cx, 0 ;contador
-cmp ax,0 ;verifica sinal
-jge R2 ;salta se número positivo
-mov bl, 2Dh ;senão, escreve sinal –
-mov ds:[di], bl
-add di, 1 ;incrementa índice
-neg ax ;toma módulo do número
-R2:
-mov bx, 10 ;divisor
+  mov di, 02
+  mov si, 16386
+  mov ax, 0
+  mov cx, 10
+  mov dx, 1 ;valor sinal +
+  mov bh, 0
+  mov bl, ds:[di] ;caractere
+  cmp bx, 2Dh ;verifica sinal
+  jne R0 ;se nï¿½o negativo
+  mov dx, -1 ;valor sinal -
+  add di, 1 ;incrementa base
+  mov bl, ds:[di] ;prï¿½ximo caractere
 R3:
-add cx, 1 ;incrementa contador
-mov dx, 0 ;estende 32bits p/ div.
-idiv bx ;divide DXAX por BX
-push dx ;empilha valor do resto
-cmp ax, 0 ;verifica se quoc. é 0
-jne R3 ;se não é 0, continua
-;agora, desemp. os valores e escreve o string
+  push dx ;empilha sinal
+  mov dx, 0 ;reg. multiplicaï¿½ï¿½o
 R4:
-pop dx ;desempilha valor
-add dx, 30h ;transforma em caractere
-mov ds:[di],dl ;escreve caractere
-add di, 1 ;incrementa base
-add cx, -1 ;decrementa contador
-cmp cx, 0 ;verifica pilha vazia
-jne R4 ;se não pilha vazia, loop
-;grava fim de string
-mov dl, 024h ;fim de string
-mov ds:[di], dl ;grava '$'
-;exibe string
-mov dx, 0
-mov ah, 09h
-int 21h
+  cmp bx, 0dh ;verifica fim string
+  je R2 ;salta se fim string
+  imul cx ;mult. 10
+  add bx, -48 ;converte caractere
+  add ax, bx ;soma valor caractere
+  add di, 1 ;incrementa base
+  mov bh, 0
+  mov bl, ds:[di] ;prï¿½ximo caractere
+  jmp R1 ;loop
+R5:
+  pop cx ;desempilha sinal
+  imul cx ;mult. sinal
 
-mov ah, 02h
-mov dl, 0Dh
-int 21h
-mov DL, 0Ah
-int 21h
+  mov al, 0FFh ; const true
+  mov DS:[0], al
+  mov al, DS:[0]
+  mov DS:[16642], ax
+  mov al, 0 ; const 0
+  mov DS:[0], al
+  mov al, DS:[0]
+  mov ah, 0
+  mov DS:[16384], ax
+R6:
+  mov ax, DS:[16642]
+  cmp ax, 0
+  je R1
+  mov di, 0 ;end. string temp.
+  mov cx, 0 ;contador
+  cmp ax,0 ;verifica sinal
+  jge R2 ;salta se nï¿½mero positivo
+  mov bl, 2Dh ;senï¿½o, escreve sinal ï¿½
+  mov ds:[di], bl
+  add di, 1 ;incrementa ï¿½ndice
+  neg ax ;toma mï¿½dulo do nï¿½mero
+R7:
+  mov bx, 10 ;divisor
+R8:
+  add cx, 1 ;incrementa contador
+  mov dx, 0 ;estende 32bits p/ div.
+  idiv bx ;divide DXAX por BX
+  push dx ;empilha valor do resto
+  cmp ax, 0 ;verifica se quoc. ï¿½ 0
+  jne R3 ;se nï¿½o ï¿½ 0, continua
+  ;agora, desemp. os valores e escreve o string
+R9:
+  pop dx ;desempilha valor
+  add dx, 30h ;transforma em caractere
+  mov ds:[di],dl ;escreve caractere
+  add di, 1 ;incrementa base
+  add cx, -1 ;decrementa contador
+  cmp cx, 0 ;verifica pilha vazia
+  jne R4 ;se nï¿½o pilha vazia, loop
+  ;grava fim de string
+  mov dl, 024h ;fim de string
+  mov ds:[di], dl ;grava '$'
+  ;exibe string
+  mov dx, 0
+  mov ah, 09h
+  int 21h
+
+  mov ah, 02h
+  mov dl, 0Dh
+  int 21h
+  mov DL, 0Ah
+  int 21h
 
 
-dseg SEGMENT PUBLIC
-byte "ola'$"
-dseg ENDS
+  dseg SEGMENT PUBLIC
+  byte "ola'$"
+  dseg ENDS
 
-mov al, 1 ; const 1
-mov DS:[0], al
-mov ax, DS:[16384]
-mov bx, DS:[0]
-add ax, bx ; plus
-cwd ; converter pra inteiro
-mov DS:[0], ax
-mov al, DS:[1]
-mov DS:[16384], ax
-mov ax, DS:[16384]
-cwd
-mov cx, ax
-mov ax, DS:[16642]
-cwd
-mov bx, ax
-mov ax, cx
+  mov al, 1 ; const 1
+  mov DS:[0], al
+  mov ax, DS:[16384]
+  mov bx, DS:[0]
+  add ax, bx ; plus
+  cwd ; converter pra inteiro
+  mov DS:[0], ax
+  mov al, DS:[1]
+  mov DS:[16384], ax
+  mov ax, DS:[16384]
+  cwd
+  mov cx, ax
+  mov ax, DS:[16642]
+  cwd
+  mov bx, ax
+  mov ax, cx
 
-cmp ax, bx
-jl R0
-mov AL, 0
-jmp R1
-R0:
-mov AL, 0FFh
-R1:
-mov DS:[0], AL
-mov al, DS:[0]
-mov DS:[16642], ax
-jmp R0
-R1:
-mov ah,4Ch
-int 21h
-cseg ENDS ;fim seg. código
+  cmp ax, bx
+  jl R0
+  mov AL, 0
+  jmp R1
+R10:
+  mov AL, 0FFh
+R11:
+  mov DS:[0], AL
+  mov al, DS:[0]
+  mov DS:[16642], ax
+  jmp R0
+R12:
+  mov ah,4Ch
+  int 21h
+cseg ENDS ;fim seg. cï¿½digo
 END strt ;fim programa
